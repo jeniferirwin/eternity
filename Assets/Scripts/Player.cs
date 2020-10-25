6f2c803909e2f9ad0fc4c780522ac27763d0c5ce
jeniferirwin <jenifer.irwin@gmail.com>
2020-10-25 04:43:12 -0400

    I made my first animated 3D character model!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ namespace Eternity
     public class Player : MonoBehaviour
     {
         public float moveSpeed;
+        public Animator playerAnimator;
+        public GameObject modelContainer;
 
         private bool isSafe;
         private Modifiers modifiers;
@@ -28,8 +30,37 @@ namespace Eternity
         {
             float haxis = Input.GetAxis("Horizontal");
             float vaxis = Input.GetAxis("Vertical");
+            if (haxis != 0 || vaxis != 0)
+            {
+                playerAnimator.SetBool("isWalking", true);
+            }
+            else
+            {
+                playerAnimator.SetBool("isWalking", false);
+            }
+            
+            Vector3 lookTarget = new Vector3(0,0,0);
+            if (haxis > 0)
+            {
+                lookTarget.x = 1;
+            }
+            else if (haxis < 0)
+            {
+                lookTarget.x = -1;
+            }
+
+            if (vaxis > 0)
+            {
+                lookTarget.z = 1;
+            }
+            else if (vaxis < 0)
+            {
+                lookTarget.z = -1;
+            }
+
 
             transform.Translate(new Vector3(haxis, 0, vaxis) * moveSpeed * Time.deltaTime * (1 + modifiers.fragmentsGathered / 50));
+            modelContainer.transform.LookAt(transform.position + lookTarget);
         }
 
         private void Die()
